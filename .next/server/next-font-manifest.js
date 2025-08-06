4e68afedc6d6ab04496d50fc5eb555053642a6a6
meta-kk <11097094+teacher-kk@user.noreply.gitee.com>
2025-08-06 15:06:45 +0800

    init

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/xukaikai/Desktop/Vision/joycards/app/layout\":[\"static/media/66f30814ff6d7cdf.p.woff2\",\"static/media/e11418ac562b8ac1-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
